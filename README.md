42e39a9d93ba6005088c446037fca0dde5318990
JJCervera <jjcervera05@gmail.com>
2018-09-14 08:23:10 -0300

    "correcciones menores"

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ La conexión se realiza mediante un Hardware ID (MAC) y contraseña de alumno. P
 
 OJO!: *** NO SE DEBE UTILIZAR UN USUARIO ADMINISTRADOR NI OPERADOR DEL TDSERVER ****
 
-Editar el archivo y modificar en la parte de declaración de valores de las variables:
+configuraciones posibles editando el archivo "config.js"
 
 
-Si lo prefieren pueden renombrar el archivo "desb_index.html" a solo "index.html" así los navegadores lo cargan automáticamente al entrar a la dirección "http://servicios/tdserveralumno" o "http://172.16.0.1/tdserveralumno" (como ejemplo) en el servidor Apache.
+
--- config.js
+++ config.js
@@ -5,5 +5,6 @@
 var IP_tdserver = "172.16.0.2";  			// default: 172.16.0.2 
 var MAC = "AABBCCDDEEFF";					// hay que crear una cuenta alumno para una MAC (recomendado de una netbook en desuso).
 var Contrasena = "Alumno.";					// va la contraseña generada para la cuenta del alumno de la MAC elegida.
-var ContrasenaEncriptada = true;			// si hay error de login durante la carga de la página es probable que el TDServer trabaje con contraseña encriptada.
-											// probar con los valores =   true o false
+var ContrasenaEncriptada = false;			// true o false : Si hay error de login durante la carga de la página es probable que el TDServer 
+											// sea mas una versión mas nueva y  trabaje con contraseña encriptada, en cuyo caso cambiar a "true"
+											
